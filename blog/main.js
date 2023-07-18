8b261c19a6b1704e9be73040bb8e658a8f81f0c4
Zhang que <112451217+copilotuser1@users.noreply.github.com>
2023-07-18 17:47:02 +0800

    1

--- blog/main.js
+++ blog/main.js
@@ -11,4 +11,9 @@ if(process.argv.slice(2)[0].trim() === 'init') {
   spacePath = process.argv.slice(2)[2] || ''
  }
  startServe(spacePath)
+}else if( process.argv.slice(2)[0].trim() === 'build'){
+ if(process.argv.slice(2)[1] === '-p') {
+  spacePath = process.argv.slice(2)[2] || ''
+ }
+ startServe(spacePath)
 }
--- blog/ssr-md.config.js
+++ blog/ssr-md.config.js
@@ -12,10 +12,6 @@ module.exports = {
  markdown_theme: 'themes/github-markdown', // md主题,文件路径对应static下的css文件
  highlight_theme: 'highlight/styles/vs2015.min', // 代码高亮主题，来自highlight.js主题
  static_fold: 'static', // 你的静态文件资源站,不要将项目根目录作为静态资源站，会很危险哦
- custom_css: "css/md_css", // md页的自定义css地址(可以是字符串也可以是字符串数组),如果要使用自定义css，则需要设置static_fold（静态资源文件夹），并将css文件放到static_fold对应的文件夹下
- custom_hi_css: "", // 登录页的自定义css地址(可以是字符串也可以是字符串数组),如果要使用自定义css，则需要设置static_fold（静态资源文件夹），并将css文件放到static_fold对应的文件夹下
- custom_js: "", // md页的自定义js地址(可以是字符串也可以是字符串数组),如果要使用自定义js，则需要设置static_fold（静态资源文件夹），并将js文件放到static_fold对应的文件夹下
- custom_hi_js: "", // 登录页的自定义js地址(可以是字符串也可以是字符串数组),如果要使用自定义js，则需要设置static_fold（静态资源文件夹），并将js文件放到static_fold对应的文件夹下
  // todo
  admin_password: '', // 管理员密码，可以进行密码管理，通过修改.passwords文件,必须有管理员cookie
  // 密码管理todo，暂未实现
